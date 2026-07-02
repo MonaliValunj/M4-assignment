a963862d9ba7694c0fe606fbd2d5e39fbe8d5f00
Monali Valunj <monavalunj96@gmail.com>
2026-07-01 17:32:20 -0700

    function practive step 2 done

--- js/01-functions-practice.js
+++ js/01-functions-practice.js
@@ -1,6 +1,18 @@
 //STEP 1
+// function halfNumber(number) {
+//     let result = number / 2
+//     console.log("Half of " + number + " is " + result + ".")
+//     return result
+// }
+// halfNumber(5)
 
 //STEP 2
+// function squareNumber(number) {
+//     let result = number * number
+//     console.log("The result of squaring the number " + number + " is " + result + ".")
+//     return result
+// }
+// squareNumber(3)
 
 //STEP 3
 
